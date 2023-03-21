eeed93afabce4ab41e08596c9d867e62eddda689
tushar_madheshia <tushar_madheshia>
2023-03-21 07:37:35 +0000

    auto save by knights-watch

--- runner.py
+++ runner.py
@@ -1,7 +1,6 @@
-from transformers import pipeline, set_seed
-
-generator = pipeline('text-generation', model='openai-gpt')
-
-set_seed(42)
-
-generator("The man worked as a", max_length=10, num_return_sequences=5)
+def run():
+    from transformers import pipeline, set_seed
+    generator = pipeline('text-generation', model='openai-gpt')
+    set_seed(42)
+    x = generator("The man worked as a", max_length=10, num_return_sequences=5)
+    return x
